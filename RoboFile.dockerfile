eabc725964066eacb4e809bb07d2df99ee5810d8
MayraEge <mayra.ege@btc-ag.com>
2025-02-25 11:51:37 +0100

    Docker Connection

---
+++ RoboFile.dockerfile
@@ -0,0 +1,5 @@
+FROM amazoncorretto:21
+ARG JAR_FILE=target/RobotWarsServer-1.0-SNAPSHOT.jar
+ADD ${JAR_FILE} app.jar
+EXPOSE 8080
+ENTRYPOINT ["java", "-jar", "/app.jar"]
--- src/main/java/Services/Main.java
+++ src/main/java/Services/Main.java
@@ -2,12 +2,12 @@ package Services;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = {"DB"})
 @EntityScan(basePackages = {"Model"})
 public class Main{
-    public static void main(String[] args) {    SpringApplication.run(Main.class, args);}
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);}
 }
